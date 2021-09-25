2b99a8405e48a89199c62b7a8e2acec992ab94bb
DoodleBob <riley.foust18@gmail.com>
2021-09-25 14:41:43 -0400

    added button functionality

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,25 +2,82 @@ var currentDayEl = document.querySelector("#currentDay");
 var todayDate = moment().format("MMMM Do YYYY");
 currentDayEl.textContent = todayDate;
 
+var items = [];
+var setHourIndex = 9;
+
 var setTime = function() {
-  var setHourIndex = 9;
   for (var i = 0; i < 9; i++) {
-    var textarea = document.querySelector(`#textarea${i}`);
+    var textareaEl = document.querySelector(`#textarea${i}`);
     var hour = document.querySelector(`#hour${i}`);
     var setHour = moment().set("hour", setHourIndex);
     hour.textContent = setHour.format("hA");
     setHourIndex++;
     if (Math.floor(moment().diff(setHour, "hours")) === 0) {
-      textarea.classList.add("present");
+      textareaEl.classList.add("present");
     }
     else if (Math.floor(moment().diff(setHour, "hours")) > 0) {
-      textarea.classList.add("past");
+      textareaEl.classList.add("past");
     }
     else if (Math.floor(moment().diff(setHour, "hours")) < 0) {
-      textarea.classList.add("future");
+      textareaEl.classList.add("future");
     }
   }
 }
 
+// var loadTasks = function() {
+//   tasks = JSON.parse(localStorage.getItem("tasks"));
+
+//   // if nothing in localStorage, create a new object to track all task status arrays
+//   if (!tasks) {
+//     tasks = {
+//       toDo: [],
+//       inProgress: [],
+//       inReview: [],
+//       done: []
+//     };
+//   }
+
+//   // loop over object properties
+//   $.each(tasks, function(list, arr) {
+//     // then loop over sub-array
+//     arr.forEach(function(task) {
+//       createTask(task.text, task.date, list);
+//     });
+//   });
+// };
+
+var loadItems = function() {
+  items = JSON.parse(localStorage.getItem("items"));
+  if (!items) {
+    items = {
+      nineAM: [],
+      tenAM: [],
+      elevenAM: [],
+      twelevePM: [],
+      onePM: [],
+      twoPM: [],
+      threePM: [],
+      fourPM: [],
+      fivePM: [],
+    }
+    localStorage.setItem("items", JSON.stringify(items));
+  }
+
+}
+
+var changeItems = function(textInput, textId) {
+  console.log(textInput);
+  console.log(textId);
+}
+
+
+$(".row").on("click", ".saveBtn", function(event) {
+  var textSelect = $(this).parent().children()[1];
+  var textInput = textSelect.value.trim();
+  var textId = textSelect.id;
+  changeItems(textInput, textId);
+})
+
 setTime();
+loadItems();
 
